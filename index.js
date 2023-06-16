ea1720953441400aacb8b01d23cdc56a3b8a3d17
Cosmin Negrut <cosmin.negrut@gmail.com>
2023-06-16 20:55:45 +0300

    use activePage to store last visible page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+var activePage = "home";
+
+// utilitis functions
+
 function hide(id) {
   console.info("hide %o element", id);
   document.getElementById(id).style.display = "none";
 }
 
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
-}
-
 function showPage(id) {
-  hideAllPages();
+  hide(activePage);
   var page = document.getElementById(id);
-  console.info("show %o ...", id, page);
+  console.info("show %o", id, page);
   page.style.display = "block";
+  activePage = id;
 }
 
-showPage("home");
+// start our code
+
+showPage(activePage);
