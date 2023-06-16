93c3bb62eaec9e7c0dd3fae88665129a46e509a3
Cosmin Negrut <cosmin.negrut@gmail.com>
2023-06-16 20:01:09 +0300

    remove all showX functions and use only showPage (id)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function showHome() {
-  var page = document.getElementById("home");
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+function showPage(id) {
+  hideAllPages();
+  var page = document.getElementById(id);
+  console.info("show", id, page);
   page.style.display = "block";
 }
 
-function showSkills() {
-  var page = document.getElementById("skills");
-  page.style.display = "block";
-}
-
-showHome();
+showPage("home");
