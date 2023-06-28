1b919ca3393a41f07836f4e02258ce0eb9947523
Cosmin Negrut <cosmin.negrut@gmail.com>
2023-06-28 16:35:20 +0300

    changes to the cv

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ function clickOnMenu(e) {
   if (link) {
     var id = link.dataset.page;
     //console.warn("click %o menu", e.target.getAttribute("data-page"));
+    //console.warn("click %o menu", id, e.target.matches("a"));
     //console.warn("click %o menu", id);
     if (id) {
+      //if (e.target.matches("a") & id) {
       showPage(id);
     }
   }
@@ -55,9 +57,11 @@ function sortByName(a, b) {
 function showSkills(skills) {
   skills.sort(sortByEndorcements);
   var htmlSkills = skills.map(function (skill) {
+    console.info("skill", skill);
     var cls = skill.favorite ? "favorite" : "";
     return `<li class="${cls}">${skill.name} <span>- ${skill.endorcements}</span></li>`;
   });
+  console.info("skill", htmlSkills);
   var ul = $("#skills ul");
   ul.innerHTML = htmlSkills.join("");
 }
@@ -72,8 +76,51 @@ function loadSkills() {
   });
 }
 
+//HR Skills
+
+function showHrSkills(skills) {
+  var htmlSkills = skills.map(function (skill) {
+    console.info("skill", skill);
+    return `<li>${skill.name}</li>`;
+  });
+  console.info("skills", htmlSkills);
+  var ul = $("#hrSkills");
+  ul.innerHTML = htmlSkills.join("");
+}
+
+function loadHrSkills() {
+  var response = fetch("hrskills.json");
+  var loaded = response.then(function (r) {
+    return r.json();
+  });
+  loaded.then(function (skills) {
+    showHrSkills(skills);
+  });
+}
+
+function showTableOfFour() {
+  var checkBox = document.getElementById("four");
+  var table = document.getElementById("tableOfFour");
+  if (checkBox.checked == true) {
+    table.style.display = "block";
+  } else {
+    table.style.display = "none";
+  }
+}
+
+function showTableOfNine() {
+  var checkBox = document.getElementById("nine");
+  var table = document.getElementById("tableOfNine");
+  if (checkBox.checked == true) {
+    table.style.display = "block";
+  } else {
+    table.style.display = "none";
+  }
+}
+
 // start our code
 
 showPage(activePage);
 $("#top-menu-bar").addEventListener("click", clickOnMenu);
 loadSkills();
+loadHrSkills();
